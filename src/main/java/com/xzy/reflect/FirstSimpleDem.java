639f2e674a9948213c71ed58a646ac48f326a56d
RuzzZZ <xzy199400@foxmail.com>
2017-02-24 17:21:34 +0800

    update .gitignore

--- src/main/java/com/xzy/reflect/FirstSimpleDem.java
+++ src/main/java/com/xzy/reflect/FirstSimpleDem.java
@@ -1,11 +1,16 @@
 package com.xzy.reflect;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 /**
  * Created by RuzzZZ on 2017/2/20.
  */
 public class FirstSimpleDem {
+
+    public Integer field1;
+
+    public String field2;
 
     public void sayHi() {
 
@@ -22,15 +27,25 @@ public class FirstSimpleDem {
     public static void main(String[] args) throws ClassNotFoundException {
 //        Class clazz = FirstSimpleDem.class;
         Class clazz = Class.forName("com.xzy.reflect.FirstSimpleDem");
+        System.out.println("类名：" + clazz.getSimpleName());
+        System.out.println("全限定类名：" + clazz.getName());
+
+        Field[] fields = clazz.getFields();
+        for (Field field : fields) {
+            System.out.println("==============================Field start==============================");
+            System.out.println(field.getName() + "\t" + field.getType().getName());
+            System.out.println("==============================Field end==============================");
+        }
+
         Method[] methods = clazz.getMethods();
         for (Method method : methods) {
-            System.out.println("==============================start==============================");
+            System.out.println("==============================Method start==============================");
             System.out.println(method.getName() + ";the parameterTypes");
             Class[] parameterTypes = method.getParameterTypes();
             for (Class param : parameterTypes) {
                 System.out.println(param.getName());
             }
-            System.out.println("==============================end==============================");
+            System.out.println("==============================Method end==============================");
         }
     }
 }
