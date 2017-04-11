3aa49c275b010b51dab5417a4098c490fcb0de1e
RuzzZZ <xzy199400@foxmail.com>
2017-04-11 17:14:17 +0800

    Test URLConnnection

--- src/main/java/com/xzy/collections/SubListDemo.java
+++ src/main/java/com/xzy/collections/SubListDemo.java
@@ -1,7 +1,12 @@
 package com.xzy.collections;
 
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 import static java.lang.System.out;
 /**
@@ -11,24 +16,42 @@ import static java.lang.System.out;
  */
 public class SubListDemo {
 
-    public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-        list.add("qqqq1");
-        list.add("qqqq2");
-        list.add("qqqq3");
-        list.add("qqqq4");
-        list.add("qqqq5");
-        list.add("qqqq6");
-        list.add("qqqq7");
-        list.add("qqqq8");
-        List<String> subList = list.subList(0, list.size());
-        subList.clear();
-        subList.add("Hello1");
-        subList.add("Hello2");
-        subList.add("Hello3");
-        out.println(list.size());
-        out.println(subList.size());
+    public static void main(String[] args) throws IOException {
+//        List<String> list = new ArrayList<>();
+//        list.add("qqqq1");
+//        list.add("qqqq2");
+//        list.add("qqqq3");
+//        list.add("qqqq4");
+//        list.add("qqqq5");
+//        list.add("qqqq6");
+//        list.add("qqqq7");
+//        list.add("qqqq8");
+//        List<String> subList = list.subList(0, list.size());
+//        subList.clear();
+//        subList.add("Hello1");
+//        subList.add("Hello2");
+//        subList.add("Hello3");
+//        out.println(list.size());
+//        out.println(subList.size());
 
+
+        URL url = new URL("http://merge-3d.gxpan.cn/uploads/model/chara_board/338126b930c5aebe2584f7d164d4e02d/中国版-哪吒-樱花.zip");
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        InputStream inputStream = conn.getInputStream();
+
+        BufferedInputStream buff = new BufferedInputStream(inputStream);
+
+
+        FileOutputStream fos = new FileOutputStream("中国版-哪吒-樱花.zip");
+        byte[] b = new byte[1024];
+
+        while((inputStream.read(b)) != -1){
+
+            fos.write(b);
+
+        }
     }
 
 }
