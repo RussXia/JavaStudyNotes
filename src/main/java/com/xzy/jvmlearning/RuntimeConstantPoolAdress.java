9df0af2c7fec6dd24f35d25f1e684ca7bb6370cd
RuzzZZ <xzy199400@foxmail.com>
2017-11-27 09:54:12 +0800

    JVM  oom

---
+++ src/main/java/com/xzy/jvmlearning/RuntimeConstantPoolAdress.java
@@ -0,0 +1,16 @@
+package com.xzy.jvmlearning;
+
+/**
+ * @author RuzzZZ
+ * @since 17/11/2017 4:27 PM
+ */
+public class RuntimeConstantPoolAdress {
+
+    static final String TEST_WORLD = "test world";
+
+    public static void main(String[] args) {
+        String str = new String("test world");
+        System.out.println(str.intern() == TEST_WORLD);
+        System.out.println(str.intern() == str);
+    }
+}
